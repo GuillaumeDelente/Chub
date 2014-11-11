64910ff5ba66c52408ba5a48d9bd13047defe4b7
Guillaume Delente <guillaume.delente+github@gmail.com>
2014-11-10 23:45:51 -0800

    Search box

--- app/src/main/java/android/chub/io/chub/fragment/MapFragment.java
+++ app/src/main/java/android/chub/io/chub/fragment/MapFragment.java
@@ -1,17 +1,32 @@
 package android.chub.io.chub.fragment;
 
+import android.chub.io.chub.BuildConfig;
 import android.chub.io.chub.R;
 import android.chub.io.chub.data.api.GeocodingService;
 import android.chub.io.chub.data.api.model.GoogleAddress;
 import android.chub.io.chub.data.api.model.GoogleResponse;
+import android.chub.io.chub.util.DialerUtils;
+import android.chub.io.chub.widget.ActionBarController;
+import android.chub.io.chub.widget.SearchEditTextLayout;
+import android.content.res.Resources;
 import android.graphics.Outline;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -32,13 +47,20 @@ import rx.schedulers.Schedulers;
 /**
  * Created by guillaume on 11/9/14.
  */
-public class MapFragment extends BaseFragment {
+public class MapFragment extends BaseFragment implements ActionBarController.ActivityUi {
 
+    private static final String TAG = "MapFragment";
     private MapView mMapView;
     private ImageButton mShareLocationFab;
-    private SearchView mSearchView;
+    private ActionBarController mActionBarController;
+    private EditText mSearchView;
+    //private SearchEditTextLayout mSearchView;
     @Inject
     GeocodingService mGeocodingService;
+    private int mActionBarHeight;
+    private String mSearchQuery;
+    private boolean mInRegularSearch;
+    private FrameLayout mParentLayout;
 
 
     @Override
@@ -52,7 +74,7 @@ public class MapFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         mMapView = (MapView) view.findViewById(R.id.mapview);
         mShareLocationFab = (ImageButton) view.findViewById(R.id.share_location_fab);
-        mSearchView = (SearchView) view.findViewById(R.id.search_view);
+        //mSearchView = (SearchEditTextLayout) view.findViewById(R.id.search_view);
         final int size = getResources().getDimensionPixelSize(R.dimen.fab_size);
         mShareLocationFab.setOutlineProvider(new ViewOutlineProvider() {
             @Override
@@ -71,6 +93,32 @@ public class MapFragment extends BaseFragment {
                 mMapView.setCenter(myLocationOverlay.getMyLocation());
             }
         });
+
+        mParentLayout = (FrameLayout) view.findViewById(R.id.container);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ActionBarActivity activity = ((ActionBarActivity) getActivity());
+        ActionBar actionBar = activity.getSupportActionBar();
+        actionBar.setCustomView(R.layout.search_edittext);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setBackgroundDrawable(null);
+        final Resources resources = activity.getResources();
+        mActionBarHeight = resources.getDimensionPixelSize(R.dimen.action_bar_height_large);
+        mActionBarController = new ActionBarController(this,
+                (SearchEditTextLayout) actionBar.getCustomView());
+        final SearchEditTextLayout searchEditTextLayout =
+                (SearchEditTextLayout) actionBar.getCustomView();
+        searchEditTextLayout.setPreImeKeyListener(mSearchEditTextLayoutListener);
+        mSearchView = (EditText) searchEditTextLayout.findViewById(R.id.search_view);
+        mSearchView.addTextChangedListener(mPhoneSearchQueryTextListener);
         mMapView.addListener(new MapListener() {
             @Override
             public void onScroll(ScrollEvent scrollEvent) {
@@ -78,18 +126,19 @@ public class MapFragment extends BaseFragment {
                 mGeocodingService.getAddress(String.format("%f,%f", center.getLatitude(),
                         center.getLongitude()))
                         .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<GoogleResponse<GoogleAddress>>() {
-                    @Override
-                    public void call(GoogleResponse<GoogleAddress> googleAddressGoogleResponse) {
-                        if (googleAddressGoogleResponse.results.isEmpty()) {
-                            mSearchView.setQuery(null, false);
-                        } else {
-                            mSearchView.setQuery(
-                                    googleAddressGoogleResponse.results.get(0).formattedAddress, false);
-                        }
-                    }
-                });
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Action1<GoogleResponse<GoogleAddress>>() {
+                            @Override
+                            public void call(GoogleResponse<GoogleAddress> googleAddressGoogleResponse) {
+                                searchEditTextLayout.setCollapsedSearchBoxText(null);
+                                if (!googleAddressGoogleResponse.results.isEmpty()) {
+                                    searchEditTextLayout
+                                            .setCollapsedSearchBoxText(
+                                                    googleAddressGoogleResponse
+                                                            .results.get(0).formattedAddress);
+                                }
+                            }
+                        });
             }
 
             @Override
@@ -97,13 +146,186 @@ public class MapFragment extends BaseFragment {
 
             }
         });
+    }
 
+
+    /*
+    * Listener used to send search queries to the phone search fragment.
+    */
+    private final TextWatcher mPhoneSearchQueryTextListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            final String newText = s.toString();
+            if (newText.equals(mSearchQuery)) {
+                // If the query hasn't changed (perhaps due to activity being destroyed
+                // and restored, or user launching the same DIAL intent twice), then there is
+                // no need to do anything here.
+                return;
+            }
+            if (BuildConfig.DEBUG) {
+                Log.d(TAG, "onTextChange for mSearchView called with new query: " + newText);
+                Log.d(TAG, "Previous Query: " + mSearchQuery);
+            }
+            mSearchQuery = newText;
+
+            // Show search fragment only when the query string is changed to non-empty text.
+            if (!TextUtils.isEmpty(newText)) {
+                enterSearchUi(mSearchQuery);
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+        }
+    };
+
+
+
+    /**
+     * Open the search UI when the user clicks on the search box.
+     */
+    private final View.OnClickListener mSearchViewOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (!isInSearchUi()) {
+                mActionBarController.onSearchBoxTapped();
+                enterSearchUi(mSearchView.getText().toString());
+            }
+        }
+    };
+
+    /**
+     * Shows the search fragment
+     */
+    private void enterSearchUi(String query) {
+        if (getFragmentManager().isDestroyed()) {
+            // Weird race condition where fragment is doing work after the activity is destroyed
+            // due to talkback being on (b/10209937). Just return since we can't do any
+            // constructive here.
+            return;
+        }
+
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "Entering search UI - smart dial ");
+        }
+        mInRegularSearch = true;
+/*
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        if (mInDialpadSearch && mSmartDialSearchFragment != null) {
+            transaction.remove(mSmartDialSearchFragment);
+        } else if (mInRegularSearch && mRegularSearchFragment != null) {
+            transaction.remove(mRegularSearchFragment);
+        }
+
+        final String tag;
+        if (smartDialSearch) {
+            tag = TAG_SMARTDIAL_SEARCH_FRAGMENT;
+        } else {
+            tag = TAG_REGULAR_SEARCH_FRAGMENT;
+        }
+        mInDialpadSearch = smartDialSearch;
+        mInRegularSearch = !smartDialSearch;
+
+        SearchFragment fragment = (SearchFragment) getFragmentManager().findFragmentByTag(tag);
+        transaction.setCustomAnimations(android.R.animator.fade_in, 0);
+        if (fragment == null) {
+            if (smartDialSearch) {
+                fragment = new SmartDialSearchFragment();
+            } else {
+                fragment = new RegularSearchFragment();
+            }
+            transaction.add(R.id.dialtacts_frame, fragment, tag);
+        } else {
+            transaction.show(fragment);
+        }
+        // DialtactsActivity will provide the options menu
+        fragment.setHasOptionsMenu(false);
+        fragment.setShowEmptyListForNullQuery(true);
+        fragment.setQueryString(query, false);
+        transaction.commit();
+
+        mListsFragment.getView().animate().alpha(0).withLayer();
+        */
+    }
+
+    /**
+     * If the search term is empty and the user closes the soft keyboard, close the search UI.
+     */
+    private final View.OnKeyListener mSearchEditTextLayoutListener = new View.OnKeyListener() {
+        @Override
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN &&
+                    TextUtils.isEmpty(mSearchView.getText().toString())) {
+                maybeExitSearchUi();
+            }
+            return false;
+        }
+    };
+
+    /**
+     * @return True if the search UI was exited, false otherwise
+     */
+    private boolean maybeExitSearchUi() {
+        if (isInSearchUi() && TextUtils.isEmpty(mSearchQuery)) {
+            exitSearchUi();
+            DialerUtils.hideInputMethod(mParentLayout);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Hides the search fragment
+     */
+    private void exitSearchUi() {
+        // See related bug in enterSearchUI();
+        if (getFragmentManager().isDestroyed()) {
+            return;
+        }
+
+        mSearchView.setText(null);
+        mInRegularSearch = false;
+/*
+        final FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        if (mSmartDialSearchFragment != null) {
+            transaction.remove(mSmartDialSearchFragment);
+        }
+        if (mRegularSearchFragment != null) {
+            transaction.remove(mRegularSearchFragment);
+        }
+        transaction.commit();
+
+        mListsFragment.getView().animate().alpha(1).withLayer();
+        */
+        mActionBarController.onSearchUiExited();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public boolean isInSearchUi() {
+        return mInRegularSearch;
     }
 
+    @Override
+    public boolean hasSearchQuery() {
+        return !TextUtils.isEmpty(mSearchQuery);
+    }
+
+    @Override
+    public boolean shouldShowActionBar() {
+        return false;
+    }
+
+    @Override
+    public int getActionBarHeight() {
+        return mActionBarHeight;
+    }
+
+    @Override
+    public ActionBar getActionBar() {
+        return ((ActionBarActivity) getActivity()).getSupportActionBar();
+    }
 }
